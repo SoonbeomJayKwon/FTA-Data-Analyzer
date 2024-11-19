f7ba81c9f297f571651af10d1395a36d6938bfa2
Soonbeom <soonbeom91@gmail.com>
2024-11-19 17:59:43 +1030

    New file

--- Data_Exploration_Cleaning.py
+++ Data_Exploration_Cleaning.py
@@ -3,5 +3,18 @@ import pandas as pd
 file_path = 'C:/Users/soonb/OneDrive/바탕 화면/Documents/programming/Python/FTA-Data-Analyzer/countryFiles_South_Korea.xlsx'
 df = pd.read_excel(file_path, sheet_name='Sheet1')
 
+# duplicates = df[df.duplicated(['Hs Code '])]
+# print(duplicates)
+
+# print(df.columns)
+
 # print(df.head())
-print(df.describe())
+# print(df.describe())
+
+# nine_digit_rows = df[df['Hs Code '].astype(str).str.match(r'^\d{9}$')]
+# duplicate_nine_digit_rows = nine_digit_rows[nine_digit_rows.duplicated(subset=['Hs Code '], keep=False)]
+
+
+# print(duplicate_nine_digit_rows)
+
+# print(df.info)
